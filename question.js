9ecc4f366dee314820a28ea7f8bd7d8b42a96089
Bhavya Hanshi <bhavyahanshi@gmail.com>
2025-10-03 13:44:07 +0530

    problem solution

---
+++ question.js
@@ -0,0 +1,169 @@
+let questions=[
+    {
+        num:1,
+        question:"What does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        options:[
+            "Hyper Text Markup Language",
+            "Hyperlinks and Text Markup Language",
+            "Home Text Multiple Language",
+        "High Tech Markup Language"
+        ]
+    },
+    {
+        num:2,
+        question:"Which HTML element is used to define a section of a document that is intended for navigation links?",
+        answer:"nav",
+        options:[
+            "header",
+            "nav",
+            "section",
+            "footer"
+        ]
+    },
+    {
+        num:3,
+        question:"In CSS, which selector has the highest specificity?",
+        answer:"ID selector",
+        options:[
+            "ID selector",
+            "Class selector",
+            "Element selector",
+            "Universal selector"
+        ]
+    },
+    {
+        num:4,
+        question:"Which JavaScript keyword is used for declaring a variable that can be reassigned but is block-scoped?",
+        answer:"let",
+        options:[
+            "var",
+            "const",
+            "let",
+            "static"
+             ]
+    },
+    {
+        num:5,
+        question:"What are the four components of the CSS Box Model, from the innermost to the outermost?",
+        answer:"Content, Padding, Border, Margin ",
+        options:[
+            "Content, Padding, Margin, Border",
+            "Content, Padding, Border, Margin ",
+            "Margin, Border, Padding, Content",
+            "Border, Content, Padding, Margin"
+             ]
+    },
+    {
+        num:6,
+        question:"Which HTTP status code indicates that the request has succeeded and a new resource has been created as a result?",
+        answer:"201 Created",
+        options:[
+            " 200 OK",
+            "201 Created",
+            "301 Moved Permanently",
+            "400 Bad Request"
+             ]
+    },
+  {
+        num:7,
+        question:"What is the primary purpose of the `alt` attribute in an HTML `<img>` tag?",
+        answer:"To display text if the image fails to load and for screen readers.",
+        options:[
+            "To provide a caption that appears below the image.",
+            "To display text if the image fails to load and for screen readers.",
+            "To define the image's dimensions (height and width).",
+            "To provide a tooltip that appears when the user hovers over the image."
+            
+        ]
+    },
+    {
+        num:8,
+        question:"In a Flexbox container, which property is used to align flex items along the main-axis?",
+        answer:"justify-content",
+        options:[
+            "align-items",
+            "flex-direction",
+            "justify-content",
+            "align-content"
+            ]
+    },
+    {
+        num:9,
+        question:"What does SEO stands for?",
+        answer:"Search Engine Optimization",
+        options:[
+            "Search Engine Option",
+            "Style Embedded Option",
+            "Structured Engine Option",
+            "Search Engine Optimization"
+             ]
+    },
+    {
+        num:10,
+        question:"What does CSS stands for?",
+        answer:"Cascade Style Sheet",
+        options:[
+            "Computer Screen Server",
+            "Cascade Style Sheet",
+            "Contest Style Sheet",
+            "Computer Style Screen"
+             ]
+    },
+    {
+        num:11,
+        question:"Which of the three core web languages is responsible for defining the **structure and content** of a web page?",
+        answer:"HTML",
+        options:[
+            "Python",
+            "CSS",
+            "HTML",
+            "Javascript"
+        ]
+    },
+    {
+        num:12,
+        question:"Which language is used for styling of the web pages?",
+        answer:"CSS",
+        options:[
+            "CSS",
+            "C",
+            "C#",
+            "C++"
+            
+        ]
+    },
+    {
+        num:13,
+        question:" Code that runs directly in a user's web browser, handling the visual interface and interactivity, is known as:",
+        answer:"Front-End Development",
+        options:[
+            "Database Management",
+            "Server-Side Scripting",
+            "Back-End Development",
+            "Front-End Development"
+        ]
+    },
+    {
+        num:14,
+        question:" Which of these language is used for interaction to the web page?",
+        answer:"Javascript",
+        options:[
+            "HTML",
+            "Javascript",
+            "Python",
+            "CSS"
+        ]
+    },
+    {
+        num:15,
+        question:" A developer who is proficient in HTML, CSS, JavaScript (React), Python (Django), and SQL is typically referred to as a:",
+        answer:"Full-Stack Developer",
+        options:[
+            "DevOps Engineer",
+            "Web Designer",
+            "Full-Stack Developer",
+            "Front-End Specialist"
+        ]
+    }
+];
